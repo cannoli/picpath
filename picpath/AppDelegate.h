6793f5dcfbdb7af9d4b58238e8e94a09a09c5bf0
Shu <shunobi@gmail.com>
2011-12-09 14:00:50 -0800

    Initial Commit

---
+++ picpath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  picpath
+//
+//  Created by Shu Chiun Cheah on 12/9/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ picpath/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  picpath
+//
+//  Created by Shu Chiun Cheah on 12/9/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
